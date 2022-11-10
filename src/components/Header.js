f9927c401e3fc74b0325534d4646839ae2086562
sebastianAksamitowski <s.aksamitowski@gmail.com>
2022-11-11 00:39:22 +0100

    nav mobile, socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[100px] flex items-center'>
+    <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[100px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
         {/* logo */}
-        <Link to={'/'}>
+        <Link to={'/'} className='max-w-[200px]'>
           <img src={Logo} alt='' />
         </Link>
         {/* nav - initially hidden, show on desktop */}
-        <nav className='hidden lg:flex gap-x-12 font-semibold'>
+        <nav className='hidden xl:flex gap-x-12 font-semibold'>
           <Link
             to={'/'}
             className='text-[#696c6d] hover:text-primary transition'
@@ -42,11 +42,11 @@ const Header = () => {
             Contact
           </Link>
         </nav>
-        {/* socials */}
-        <Socials />
-        {/* mobile nav */}
-        <MobileNav />
       </div>
+      {/* socials */}
+      <Socials />
+      {/* mobile nav */}
+      <MobileNav />
     </header>
   );
 };
